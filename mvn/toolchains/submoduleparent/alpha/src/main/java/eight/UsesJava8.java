117354dfe8bb847f7030bb8a244655b4a7e98e37
Josh King <josh.king@xirrus.com>
2015-07-21 16:51:51 -0700

    toolchains propogate to submodules

---
+++ mvn/toolchains/submoduleparent/alpha/src/main/java/eight/UsesJava8.java
@@ -0,0 +1,40 @@
+package eight;
+
+import java.util.ArrayList;
+
+public class UsesJava8 {
+    public static void main(String[] args) {
+        new UsesJava8().willNotCompileInJava6();
+    }
+
+    private void willNotCompileInJava6() {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("java 8");
+        strings.add("allows");
+        strings.add("diamond");
+        System.out.println(strings);
+    }
+
+
+//    public void willNotCompileInJava8() throws Foo {
+//        try {
+//            throw new DaughterOfFoo();
+//        } catch (final Foo exception) {
+//            try {
+//                throw exception; // used to throw Foo, now throws DaughterOfFoo
+//            } catch (SonOfFoo anotherException) { // Reachable?	}
+//            }
+//        }
+//    }
+//
+//    class Foo extends Exception {
+//    }
+//
+//    class SonOfFoo extends Foo {
+//    }
+//
+//    class DaughterOfFoo extends Foo {
+//    }
+
+}
---
+++ mvn/toolchains/submoduleparent/hammy/src/main/java/doo/dah/OnlyCompilableInJava6.java
@@ -0,0 +1,41 @@
+package doo.dah;
+
+public class OnlyCompilableInJava6 {
+    public static void main(String[] args) {
+        try {
+            new OnlyCompilableInJava6().willNotCompileInJava8();
+        } catch (Foo foo) {
+            System.out.println(foo + " happened");
+        }
+    }
+
+//    private void willNotCompileInJava6() {
+//        ArrayList<String> strings = new ArrayList<>();
+//
+//        strings.add("java 8");
+//        strings.add("allows");
+//        strings.add("diamond");
+//        System.out.println(strings);
+//    }
+
+    public void willNotCompileInJava8() throws Foo {
+        try {
+            throw new DaughterOfFoo();
+        } catch (final Foo exception) {
+            try {
+                throw exception; // used to throw Foo, now throws DaughterOfFoo
+            } catch (SonOfFoo anotherException) { // Reachable?	}
+            }
+        }
+    }
+
+    class Foo extends Exception {
+    }
+
+    class SonOfFoo extends Foo {
+    }
+
+    class DaughterOfFoo extends Foo {
+    }
+
+}
