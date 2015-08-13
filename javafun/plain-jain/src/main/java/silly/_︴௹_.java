ecad9b9a8e7a34a7f88b109b6e1f2675da1a2065
Josh King <josh.king@xirrus.com>
2015-08-13 16:34:34 -0700

    more ridiculous things you can do in java

---
+++ javafun/plain-jain/src/main/java/silly/_︴௹_.java
@@ -0,0 +1,20 @@
+package silly;
+
+public class _︴௹_ {
+    private _︴௹_() {
+    }
+
+    private _︴௹_ _૱_() {
+        System.out.println(" ^__^ ");
+        return new _︴௹_();
+    }
+
+    public static void main(String[] args) {
+        _︴௹_ ₯ = new _︴௹_();
+        ₯.﹍_₪_$(₯);
+    }
+
+    private _︴௹_ ﹍_₪_$(_︴௹_ ﷼) {
+        return ﷼._૱_();
+    }
+}
