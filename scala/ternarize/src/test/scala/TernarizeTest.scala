41cf1b54c5cf47787991b8ee96a5d2cd81c32b48
Josh King <kingjoshdavid@gmail.com>
2015-11-17 08:49:07 -0800

    ternary sugar

---
+++ scala/ternarize/src/test/scala/TernarizeTest.scala
@@ -0,0 +1,28 @@
+import org.scalatest.{Matchers, FunSuite}
+
+
+//noinspection SpellCheckingInspection
+class TernarizeTest extends FunSuite with Matchers {
+  class IfTrue[A](b: => Boolean, t: => A) {
+    def |(f: => A) = if (b) t else f
+  }
+
+  class MakeIfTrue(b: => Boolean) {
+    def ?[A](t: => A) = new IfTrue[A](b, t)
+  }
+
+  implicit def autoMakeIfTrue(b: => Boolean): MakeIfTrue = new MakeIfTrue(b)
+
+  test("true should go to true") {
+    def test = {
+      println("returning true")
+      true
+    }
+    println("starting test")
+    val result = test ? "true" | "false"
+    println("result is " + result)
+
+    result should be("true")
+  }
+
+}
